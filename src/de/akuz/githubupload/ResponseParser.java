a0f6b9fe858ef8a4b4c27cd665f97064241d00bb
Till Klocke <till.klocke@itemis.de>
2012-02-19 16:11:21 +0100

    Fixed problem with non-String values like 'false' in response string

--- src/de/akuz/githubupload/ResponseParser.java
+++ src/de/akuz/githubupload/ResponseParser.java
@@ -21,16 +21,16 @@ public class ResponseParser {
 		String response = source.trim();
 		response = response.replace("{", "");
 		response = response.replace("}", "");
-		String[] parts = response.split("\",\"");
+		String[] parts = response.split(",\"");
 		Map<String, String> result = new HashMap<String, String>();
 		for (String s : parts) {
-			String[] pairs = s.split("\":\"");
+			String[] pairs = s.split("\":");
 			if(pairs.length == 2){
 				String name = pairs[0].replace("\"", "");
 				String value = pairs[1].replace("\"", "");
 				result.put(name, value);
 			} else {
-				throw new IllegalArgumentException("The Input is not parseable");
+				throw new IllegalArgumentException("The Input is not parseable! INVALID PART: "+s);
 			}
 		}
 		return result;
